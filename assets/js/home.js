488060bca3d2fed7804d9c96764c4d0df89a89ca
Konul Ibrahimova <konulsi@code.edu.az>
2023-03-01 17:36:38 +0400

    Sophia-App-Start

---
+++ assets/js/home.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+
+    let currency = $(".currency");
+
+    let usdBtn = $(".usd-btn");
+    let eurBtn = $(".eur-btn");
+
+
+    $(document).on("click", ".currency", function(){
+        $(".valyuta").toggleClass("d-none")
+    })
+
+    $(document).on("click", ".usd-btn", function(){
+        let usdText =$(".usd-btn").children().eq(0).text();
+        $(".currency").children().eq(0).text(usdText);
+        $(".valyuta").addClass("d-none")
+    })
+
+    $(document).on("click", ".eur-btn", function(){
+        let eurText = $(".eur-btn").children().eq(0).text();
+        $(".currency").children().eq(0).text(eurText);
+        $(".valyuta").addClass("d-none")
+     })
+
+
+
+     
+     $(document).on("click", ".language", function(e){
+        e.preventDefault();
+        $(".language-area").toggleClass("d-none")
+    })
+
+
+    $(document).on("click",".english-flag", function(){
+        let englishText = $(".english-flag").html();
+        $(".language").children().eq(0).html(englishText);
+        $(".language-area").addClass("d-none")
+    } )
+
+
+    $(document).on("click" , ".language-area button", function(){
+        let btnText = $(this).html();
+        $(".language").children().eq(0).html(btnText + `<i class="fa-solid fa-sort-down"></i>`)
+        $(".language-area").addClass("d-none")
+    })
+    
+    
+
+     
+     
+
+    
+})
