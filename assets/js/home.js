314c14cdf0db44e63e7eedcf0913ad4098ebf502
Konul Ibrahimova <konulsi@code.edu.az>
2023-03-02 15:26:33 +0400

    add new section

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,53 +1,49 @@
-$(document).ready(function(){
-
-    let currency = $(".currency");
-
-    let usdBtn = $(".usd-btn");
-    let eurBtn = $(".eur-btn");
+$(document).ready(function () {
 
 
-    $(document).on("click", ".currency", function(){
+    //USD
+
+    $(document).on("click", ".currency", function () {
         $(".valyuta").toggleClass("d-none")
+        $(".language-area").addClass("d-none")
     })
 
-    $(document).on("click", ".usd-btn", function(){
-        let usdText =$(".usd-btn").children().eq(0).text();
+    $(document).on("click", ".usd-btn", function () {
+        let usdText = $(".usd-btn").children().eq(0).text();
         $(".currency").children().eq(0).text(usdText);
         $(".valyuta").addClass("d-none")
     })
 
-    $(document).on("click", ".eur-btn", function(){
+    $(document).on("click", ".eur-btn", function () {
         let eurText = $(".eur-btn").children().eq(0).text();
         $(".currency").children().eq(0).text(eurText);
         $(".valyuta").addClass("d-none")
-     })
-
-
-
-     
-     $(document).on("click", ".language", function(e){
-        e.preventDefault();
-        $(".language-area").toggleClass("d-none")
     })
 
 
-    $(document).on("click",".english-flag", function(){
-        let englishText = $(".english-flag").html();
-        $(".language").children().eq(0).html(englishText);
-        $(".language-area").addClass("d-none")
-    } )
+
+    //language
+
+    $(document).on("click", ".language", function (e) {
+        e.preventDefault();
+        $(".language-area").toggleClass("d-none")
+        $(".valyuta").addClass("d-none")
+    })
 
 
-    $(document).on("click" , ".language-area button", function(){
+    $(document).on("click", ".language-area button", function () {
         let btnText = $(this).html();
         $(".language").children().eq(0).html(btnText + `<i class="fa-solid fa-sort-down"></i>`)
         $(".language-area").addClass("d-none")
     })
-    
-    
 
-     
-     
 
-    
+    //icons
+
+   
+
+
+
+
+
 })
