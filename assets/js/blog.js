bd3fb96fc25cd5c8bf45ecfd0a7e72eabb7918bc
Konul Ibrahimova <konulsi@code.edu.az>
2023-03-06 12:21:21 +0400

    add new section

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -102,22 +102,6 @@ $(document).ready(function () {
 
 
 
-    $(document).on("mouseover",".item .single-team-member",function(){
-
-        $(this).children().eq(1).children().eq(0).addClass("hide-social")
-        $(this).children().eq(1).children().eq(1).addClass("background")
-        $(this).children().eq(0).css("opacity", "0.5")
-    })
-
-
-    $(document).on("mouseout",".item .single-team-member",function(){
-
-        $(this).children().eq(1).children().eq(0).removeClass("hide-social")
-        $(this).children().eq(1).children().eq(1).removeClass("background")
-        $(this).children().eq(0).css("opacity", "1")
-    })
-
-
     
 
 
